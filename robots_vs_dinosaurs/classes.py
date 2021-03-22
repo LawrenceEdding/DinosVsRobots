ea9a5fbcb782eaa4406b340124c0b4acfe80a7cb
Lee X <lee52597@gmail.com>
2021-03-22 01:38:31 -0400

    finished project. Added all classes. added rounds of combat.

--- robots_vs_dinosaurs/classes.py
+++ robots_vs_dinosaurs/classes.py
@@ -8,19 +8,29 @@ import random
 class Robot:
     def __init__(self):
         self.name = ''  # assign at creation
-        self.health = random.randint(20, 40)
+        self.health = random.randint(20, 40)  # Bad practice Make 0 then modify with method call.
         self.power_level = 100
-        self.Weapon = self.Weapon()  # Make weapon type a class??
+        self.Weapon = Weapon()  # Make weapon type a class??
         self.attack_power = 0
         self.is_fight = False
 
     def attack(self):
         # based on weapon modifies attack power?
         # roll to hit??
+        self.power_level -= 10
         return self.attack_power
 
     def get_hit(self, damage):
         self.health -= damage
+        print(f'{self.name} has {self.health} hp left.')
+
+    def assign_attack_power(self):
+        if self.Weapon.type == 'blunt':
+            self.attack_power = 5
+        if self.Weapon.type == 'ranged':
+            self.attack_power = 7
+        if self.Weapon.type == 'piercing':
+            self.attack_power = 10
 
 
 class Dinosaur:
@@ -29,27 +39,51 @@ class Dinosaur:
         self.health = random.randint(20, 40)
         self.energy = 100
         self.attack_power = 0
-        self.is_fight = False
 
     def attack(self):
+        print(f'Choose your attack: \n'
+              f'[1] Tail slap\n'
+              f'[2] Charge\n'
+              f'[3] Bite\n'
+              f'(Press the corresponding number)\n')
+        while True:
+            response = input()
+            if response == '1':
+                self.attack_power = 5
+                break
+            if response == '2':
+                self.attack_power = 10
+                break
+            if response == '3':
+                self.attack_power = 7
+                break
+            else:
+                print(f'Please choose 1, 2, or 3')
+        self.energy -= 10
         # based on attack choice. Make choice every time from 3. Modified accuracy.
-        # roll to hit??
+        # roll to hit?? if hit return atk power else return 0
         return self.attack_power
 
     def get_hit(self, damage):
         self.health -= damage
+        print(f'{self.type} has {self.health} hp left.')
 
 
 class Fleet:
     def __init__(self):
         self.robots = []
 
+    def join_fleet(self, robot):
+        self.robots.append(robot)
+
     def robot_die(self):
         counter = 0
         while counter < len(self.robots):
             if self.robots[counter].health <= 0:
-                del self.robots[counter]
                 print(f'{self.robots[counter].name} has died.')
+                del self.robots[counter]
+
+            counter += 1
 
 
 class Weapon:
@@ -83,10 +117,16 @@ class Herd:
     def __init__(self):
         self.dinosaurs = []
 
+    def join_herd(self, dino):
+        self.dinosaurs.append(dino)
+
     def dino_die(self):
-        if self.dinosaurs[0].health == 0:
-            del self.dinosaurs[0]
-            print(f'{self.dinosaurs[0].type} has died.')
+        counter = 0
+        while counter < len(self.dinosaurs):
+            if self.dinosaurs[counter].health <= 0:
+                print(f'{self.dinosaurs[counter].type} has died.')
+                del self.dinosaurs[counter]
+            counter += 1
 
 
 class Battlefield:
@@ -95,11 +135,14 @@ class Battlefield:
         self.robot_combatant = Robot()
         self.dino_combatant = Dinosaur()
 
-    @staticmethod
-    def check_win(combatant_one, combatant_two):
-        if len(combatant_two) and len(combatant_one) == 0:
-            print(f'Draw')
-        if len(combatant_one) == 0:
-            pass
-        if len(combatant_two) == 0:
-            pass
+    # def check_win(self, combatant_one, combatant_two):
+    #     if len(combatant_two) and len(combatant_one) == 0:
+    #         print(f'Draw')
+    #     if len(combatant_one) == 0:
+    #         pass
+    #     if len(combatant_two) == 0:
+    #         pass
+
+    def enter_battlefield(self, robot, dino):
+        self.robot_combatant = robot
+        self.dino_combatant = dino
--- robots_vs_dinosaurs/main.py
+++ robots_vs_dinosaurs/main.py
@@ -16,9 +16,50 @@ if __name__ == '__main__':
 
     robot1 = Robot()
     robot2 = Robot()
-    robot3 - Robot()
+    robot3 = Robot()
 
-    
+    dino1.type = 'stegosaurus'
+    dino2.type = 't-rex'
+    dino3.type = 'pterodactyl'
 
+    robot1.name = 'ryan'
+    robot2.name = 'ryan 2.0'
+    robot3.name = 'slasher mk II'
 
+    robot1.Weapon.assign_weapon()
+    robot2.Weapon.assign_weapon()
+    robot3.Weapon.assign_weapon()
+    robot1.assign_attack_power()
+    robot2.assign_attack_power()
+    robot3.assign_attack_power()
+
+    robo_list = Fleet()
+    dino_list = Herd()
+    robo_list.join_fleet(robot1)
+    robo_list.join_fleet(robot2)
+    robo_list.join_fleet(robot3)
+    dino_list.join_herd(dino1)
+    dino_list.join_herd(dino2)
+    dino_list.join_herd(dino3)
+
+    battle = Battlefield()
+    while True:
+        battle.enter_battlefield(robo_list.robots[0], dino_list.dinosaurs[0])
+        battle.dino_combatant.get_hit(battle.robot_combatant.attack())
+        battle.robot_combatant.get_hit(battle.dino_combatant.attack())
+        robo_list.robots[0] = battle.robot_combatant
+        dino_list.dinosaurs[0] = battle.dino_combatant
+
+        robo_list.robot_die()
+        dino_list.dino_die()
+
+        if not robo_list.robots:
+            winner = 'dinosaurs'
+            break
+
+        if not dino_list.dinosaurs:
+            winner = 'robots'
+            break
+
+    print(f'{winner} has won')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
